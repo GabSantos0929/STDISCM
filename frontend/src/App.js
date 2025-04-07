9c15a2268699eb3c403567f3f703c01145df35e4
Gab Santos <63992851+GabSantos0929@users.noreply.github.com>
2025-04-07 19:05:14 +0800

    Add files via upload

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import HomePage from './pages/HomePage';
 import CourseListPage from './pages/CourseListPage';
 import DashboardPage from "./pages/DashboardPage";
 import EnrollmentPage from "./pages/EnrollmentPage";
@@ -10,7 +11,7 @@ function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<DashboardPage />} />
+        <Route path="/" element={<HomePage />} />
         <Route path="/dashboard" element={<DashboardPage />} />
         <Route path="/courses" element={<CourseListPage />} />
         <Route path="/enroll" element={<EnrollmentPage />} />
