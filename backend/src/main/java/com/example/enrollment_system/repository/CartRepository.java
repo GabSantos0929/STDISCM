47d823f1b9b5d3b2d944dc9040d70f0d08c4fc53
Gab Santos <63992851+GabSantos0929@users.noreply.github.com>
2025-04-12 00:31:56 +0800

    Delete backend/src/main/java/com/example/enrollment_system/repository/CartRepository.java

--- backend/src/main/java/com/example/enrollment_system/repository/CartRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.enrollment_system.repository;
-
-import com.example.enrollment_system.model.Cart;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CartRepository extends JpaRepository<Cart, Long> {
-    void deleteByIdAndClassNumber(int userId, String classNumber);
-}
